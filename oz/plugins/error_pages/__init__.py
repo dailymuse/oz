a0796536ce449883690a8926244e3664b710834a
Yusuf Simonson <simonson@gmail.com>
2014-12-15 13:08:44 -0500

    Use linecache for error pages so tornado templates can be easily debugged

--- oz/plugins/error_pages/__init__.py
+++ oz/plugins/error_pages/__init__.py
@@ -8,6 +8,7 @@ from .tests import *
 import traceback
 import base64
 import pprint
+import linecache
 
 class DebugBreakException(Exception):
     pass
@@ -34,19 +35,16 @@ def get_lines_from_file(filename, lineno, context_lines):
     Returns context_lines before and after lineno from file.
     Returns (pre_context_lineno, pre_context, context_line, post_context).
     """
-    
-    try:
-        with open(filename, "r") as f:
-            source = f.readlines()
 
-        lower_bound = max(0, lineno - context_lines)
-        upper_bound = lineno + context_lines
-        pre_context = [line.strip('\n') for line in source[lower_bound:lineno]]
-        context_line = source[lineno].strip('\n')
-        post_context = [line.strip('\n') for line in source[lineno + 1:upper_bound]]
-        return lower_bound, pre_context, context_line, post_context
-    except (OSError, IOError):
-        return None, [], None, []
+    get_lines = lambda s, e: [linecache.getline(filename, l).rstrip() for l in xrange(s, e)]
+
+    lower_bound = max(1, lineno - context_lines)
+    upper_bound = lineno + context_lines
+    linecache.checkcache(filename)
+    pre_context = get_lines(lower_bound, lineno)
+    context_line = linecache.getline(filename, lineno).rstrip()
+    post_context = get_lines(lineno + 1, upper_bound)
+    return lower_bound, pre_context, context_line, post_context
         
 def get_frames(tback, is_breakpoint):
     frames = []
@@ -60,7 +58,7 @@ def get_frames(tback, is_breakpoint):
         context = tback.tb_frame.f_locals
         lineno = tback.tb_lineno - 1
         tback_id = id(tback)
-        pre_context_lineno, pre_context, context_line, post_context = get_lines_from_file(filename, lineno, 7)
+        pre_context_lineno, pre_context, context_line, post_context = get_lines_from_file(filename, lineno + 1, 7)
         frames.append(ErrorFrame(tback, filename, function, lineno, context, tback_id, pre_context, context_line, post_context, pre_context_lineno))
         tback = tback.tb_next
     
