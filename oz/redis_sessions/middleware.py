3fb0c405e4f604e4f97e19a1c27631ba87e9b149
Yusuf Simonson <simonson@gmail.com>
2015-02-17 16:36:54 -0500

    Ensure the same session ID is reused on multiple lookups

--- oz/redis_sessions/middleware.py
+++ oz/redis_sessions/middleware.py
@@ -8,24 +8,28 @@ import oz.redis_sessions
 class RedisSessionMiddleware(object):
     """Adds redis-backed session capabilities"""
 
+    @property
     def _session_key(self):
         """Gets the redis key for a session"""
 
-        session_id_bytes = self.get_secure_cookie("session_id")
-        session_id = None
+        if not hasattr(self, "_cached_session_key"):
+            session_id_bytes = self.get_secure_cookie("session_id")
+            session_id = None
 
-        if session_id_bytes:
-            try:
-                session_id = session_id_bytes.decode('utf-8')
-            except:
-                pass
+            if session_id_bytes:
+                try:
+                    session_id = session_id_bytes.decode('utf-8')
+                except:
+                    pass
 
-        if not session_id:
-            session_id = oz.redis_sessions.random_hex(20)
-            self.set_secure_cookie("session_id", session_id.encode('utf-8'), httponly=True)
+            if not session_id:
+                session_id = oz.redis_sessions.random_hex(20)
+                self.set_secure_cookie("session_id", session_id.encode('utf-8'), httponly=True)
 
-        password_salt = oz.settings["session_salt"]
-        return "session:%s:v4" % oz.redis_sessions.password_hash(session_id, password_salt=password_salt)
+            password_salt = oz.settings["session_salt"]
+            self._cached_session_key = "session:%s:v4" % oz.redis_sessions.password_hash(session_id, password_salt=password_salt)
+        
+        return self._cached_session_key
 
     def _update_session_expiration(self):
         """
@@ -36,27 +40,27 @@ class RedisSessionMiddleware(object):
         session_time = oz.settings["session_time"]
 
         if session_time:
-            self.redis().expire(self._session_key(), session_time)
+            self.redis().expire(self._session_key, session_time)
 
     def get_session_value(self, name, default=None):
         """Gets a session value"""
 
-        value = self.redis().hget(self._session_key(), name) or default
+        value = self.redis().hget(self._session_key, name) or default
         self._update_session_expiration()
         return value
 
     def set_session_value(self, name, value):
         """Sets a session value"""
 
-        self.redis().hset(self._session_key(), name, value)
+        self.redis().hset(self._session_key, name, value)
         self._update_session_expiration()
 
     def clear_session_value(self, name):
         """Removes a session value"""
-        self.redis().hdel(self._session_key(), name)
+        self.redis().hdel(self._session_key, name)
         self._update_session_expiration()
 
     def clear_all_session_values(self):
         """Kills a session"""
-        self.redis().delete(self._session_key())
+        self.redis().delete(self._session_key)
         self.clear_cookie("session_id")
