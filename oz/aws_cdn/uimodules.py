e53ecd659254400dfb1c988f7aca91d1475feeba
Yusuf Simonson <simonson@gmail.com>
2016-01-12 18:01:19 -0500

    Added uimodules for referencing javascript and stylesheets with SIP

---
+++ oz/aws_cdn/uimodules.py
@@ -0,0 +1,58 @@
+import oz
+import binascii
+import tornado.web
+from tornado.escape import xhtml_escape
+
+try:
+    from urllib.parse import urlparse, parse_qs
+except ImportError:
+    from urlparse import urlparse, parse_qs
+
+class Subresource(tornado.web.UIModule):
+    def get_integrity(self, url):
+        parsed = urlparse(url)
+        query = parse_qs(parsed.query)
+
+        # cdn_static_url currently appends ?v=None to the URL if there is no
+        # known hash of the. Default to this if `v` is somehow not defined as
+        # well.
+        v_arg = query.get("v", ["None"])[0]
+
+        if v_arg == "None":
+            return None
+        else:
+            # v_arg contains the hash in hex encoding - we need to convert it
+            # to base64
+            return "sha256-%s" % binascii.b2a_base64(v_arg.decode("hex"))
+
+@oz.uimodule
+class Script(Subresource):
+    def render(self, path, sip=True, crossorigin="anonymous", **attrs):
+        url = self.handler.cdn_static_url(path)
+
+        if sip:
+            integrity = self.get_integrity(url)
+
+            if integrity:
+                attrs["integrity"] = integrity
+            
+            attrs["crossorigin"] = crossorigin
+
+        attr_strs = ['%s="%s"' % (xhtml_escape(k), xhtml_escape(v)) for (k, v) in attrs.items()]
+        return "<script %s></script>" % " ".join(attr_strs)
+
+@oz.uimodule
+class Stylesheet(Subresource):
+    def render(self, path, sip=True, **attrs):
+        url = self.handler.cdn_static_url(path)
+
+        if sip:
+            integrity = self.get_integrity(url)
+            
+            if integrity:
+                attrs["integrity"] = integrity
+        if not "rel" in attrs:
+            attrs["rel"] = "stylesheet"
+
+        attr_strs = ['%s="%s"' % (xhtml_escape(k), xhtml_escape(v)) for (k, v) in attrs.items()]
+        return "<link %s />" % " ".join(attr_strs)
