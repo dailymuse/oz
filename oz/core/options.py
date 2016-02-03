b55cd358dc0e19d14820cd4e5ad1d70491e4dab3
Yusuf Simonson <simonson@gmail.com>
2016-02-03 17:34:59 -0500

    Added body timeouts as an option

--- oz/core/options.py
+++ oz/core/options.py
@@ -14,6 +14,7 @@ oz.options(
     gzip = dict(type=bool, help="Enables gzip output."),
     wsgi_mode = dict(type=bool, default=False, help="Use WSGI mode."),
     server_workers = dict(type=int, default=1, help="The number of server workers to run concurrently."),
+    body_timeout = dict(type=int, default=None, help="How long to wait in seconds while reading the request body"),
 
     ssl_cert_file = dict(type=str, default=None, help="The SSL certificate file. If unspecified, SSL support will be disabled."),
     ssl_key_file = dict(type=str, default=None, help="The SSL key file. If unspecified, SSL support will be disabled."),
