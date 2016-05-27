c0cfea2778703acf12055c5f927a83cfd95cac64
Stephen Bly <stephenebly@gmail.com>
2016-05-27 13:04:46 -0400

    debug

--- oz/core/actions.py
+++ oz/core/actions.py
@@ -17,19 +17,4 @@ import re
 
 VALID_PROJECT_NAME = re.compile(r"^\w+$")
 
-def check_path(path, otherwise):
-    """
-    Checks if a path exists. If it does, print a warning message; if not,
-    execute the `otherwise` callback argument.
-    """
-
-    if os.path.exists(path):
-        print("WARNING: Path '%s' already exists; skipping" % path)
-    else:
-        otherwise(path)
-
-def config_maker(project_name, path):
-)
-
-    res = unittest.TextTestRunner().run(suite)
-    return 1 if len(res.errors) > 0 or len(res.failures) > 0 else 0
+def ch
